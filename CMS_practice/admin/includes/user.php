cfe21759cbe29d34690272a810e7cc1ba688120c
Sarah Kohl <Sarah@SarahBook-Air.local>
2020-05-27 07:10:40 -0400

    find user without turnery logic

--- CMS_practice/admin/includes/user.php
+++ CMS_practice/admin/includes/user.php
@@ -12,8 +12,13 @@ class User {
 
     public static function find_user_by_id($user_id) {
         global $database;
-        $result_set = self::find_this_query("SELECT * FROM users WHERE user_id = $user_id LIMIT 1");
-        $found_user = mysqli_fetch_array($result_set);
+        $the_result_array = self::find_this_query("SELECT * FROM users WHERE user_id = $user_id LIMIT 1");
+        if(!empty($the_result_array)) {
+            $first_item = array_shift($the_Result_array);
+            return $first_item;
+        } else {
+            return false;
+        }
         return $found_user;
     }
 
