abb54e6e6646111b8ab4b93da93d7fda9196e772
Sarah Kohl <Sarah@SarahBook-Air.local>
2020-06-01 07:42:52 -0400

    sessions added

--- CMS_practice/admin/includes/functions.php
+++ CMS_practice/admin/includes/functions.php
@@ -4,11 +4,13 @@ function classAutoLoader($class){
     $class = strtolower($class);
     $path = "includes/{$class}.php";
 
-    if(file_exists($path)){
-        require_once($path);
-    } else {
-        die("My dude, the file named {$class}.php cannot be found.");
+    if(is_file($path) && !class_exists($class)) {
+        include $path;
     }
+}
+
+function redirect($location) {
+    header("Location: {$location}}");
 }
 
 spl_autoload_register('classAutoLoader');
