937bb630317a86a90cfb17b18b22bcbd3ab62501
Sarah Kohl <Sarah@SarahBook-Air.local>
2020-06-15 18:23:54 -0400

    edit photo page

--- CMS_practice/admin/includes/footer.php
+++ CMS_practice/admin/includes/footer.php
@@ -7,6 +7,10 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    <!-- TinyMCE WYSIWYG Editor -->
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="js/scripts.js"></script>
+
 </body>
 
 </html>
--- CMS_practice/admin/includes/photo.php
+++ CMS_practice/admin/includes/photo.php
@@ -2,11 +2,13 @@
 
 class Photo extends Db_object {
     protected static $db_table = "photos";
-    protected static $db_fields = array('id','title','description', 'filename', 'type', 'size');
+    protected static $db_fields = array('id','title', 'caption', 'description', 'filename', 'alt_text', 'type', 'size');
     public $id;
     public $title;
+    public $caption;
     public $description;
     public $filename;
+    public $alt_text;
     public $type;
     public $size;
 
